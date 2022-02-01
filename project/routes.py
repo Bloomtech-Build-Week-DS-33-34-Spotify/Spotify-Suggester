65cf31f3b56ecc4ea342fcf35826f86976c362db
Abdel Balla <92558174+amballa@users.noreply.github.com>
2022-01-31 22:22:30 -0800

    Scaffold (#3)
    * created a barebone template for the app
    
    * incorporated the provided template to our front-end

--- project/routes.py
+++ project/routes.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from flask_assets import Bundle, Environment
+
 
 
 def build_app():
 
     app = Flask(__name__)
+
+    # To use the provided javascript and css template
+    js = Bundle('breakpoints.min.js', 'browser.min.js',
+                'jquery.min.js', 'jquery.scrolly.min.js', 'main.js',
+                'util.js', output='gen/all.js')
+
+    css = Bundle('fontawesome-all.min.css',
+                 'main.css', output='gen/all.css')
+
+    assets = Environment(app)
+
+    assets.register('all_js', js)
+    assets.register('all_css', css)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -30,14 +45,14 @@ def build_app():
         title = "Welcome to the Spotify Song Suggester app"
         description = "It's a work in progress"
         DB.create_all()
-        return render_template(
-            'home.html', title=title, description=description)
+        return render_template('index.html')
+
 
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template(
-            'home.html', description='Database has been reset')
+        return 'Database has been reset'
+
 
     return app
