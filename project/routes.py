dc2549bd316b8fe68f73b76d1bf9c04abe186aee
Abdel Balla <92558174+amballa@users.noreply.github.com>
2022-02-02 19:45:39 -0800

    fleshing out app (#7)

--- project/routes.py
+++ project/routes.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from flask_assets import Bundle, Environment
+from .api import get_track_features
+from .models import get_rec_track
 
 
 def build_app():
@@ -21,41 +23,30 @@ def build_app():
     assets.register('all_js', js)
     assets.register('all_css', css)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    DB = SQLAlchemy(app)
+    #DB = SQLAlchemy(app)
 
-    class Song(DB.Model):
-        id = DB.Column(DB.BigInteger, primary_key=True, nullable=False)
-        name = DB.Column(DB.String(25), nullable=False)
-        artist = DB.Column(DB.String(25), nullable=False)
-        # ..........
-        # ..........
-        # ..........
-
-        def __repr__(self):
-            return f"{self.name} by {self.artist}"
-
-    DB.init_app(app)
+    # DB.init_app(app)
 
     @app.route('/')
     def root():
-        title = "Welcome to the Spotify Song Suggester app"
-        description = "It's a work in progress"
-        DB.create_all()
         return render_template('index.html')
 
     @app.route('/rec', methods=['POST'])
     def recommend():
-        user_input = request.values['text_input']
-        # do data science here
-        return render_template('rec.html')
+        user_text = request.values['text_input']
+        if "by" not in user_text:
+            message = "make sure to include the artist!"
+            return render_template('index.html', message=message)
+        else:
+            (user_track_name, user_track_artist, user_track_id,
+             user_track_features) = get_track_features(user_text)
 
-    @app.route('/reset')
-    def reset():
-        DB.drop_all()
-        DB.create_all()
-        return 'Database has been reset'
+            (rec_track_name, rec_track_artist, rec_track_id) = get_rec_track(
+                user_track_id, user_track_features)
+            return render_template('rec.html', user_track=user_track_name, user_artist=user_track_artist,
+                                   rec_track=rec_track_name, rec_artist=rec_track_artist)
 
     return app
